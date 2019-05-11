ac21d9eef1ca194715e95249bc3e29a2d1283a2b
gaurangpateriya <pateriyagaurang1999@gmail.com>
2019-05-11 15:35:22 +0530

    this is the website

---
+++ precache-manifest.1d7ef2f180bd0e32ceeb37e4043c9f16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f395f23a8d9f71975a29b5a2779cd3fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "274974cf3cc148d50e8e",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "3892f8b1480888dbb34a",
+    "url": "/static/css/main.10137dd6.chunk.css"
+  },
+  {
+    "revision": "274974cf3cc148d50e8e",
+    "url": "/static/js/2.680f6e47.chunk.js"
+  },
+  {
+    "revision": "3892f8b1480888dbb34a",
+    "url": "/static/js/main.0ea52af5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2b2eca7ff6bc9fba90ad991bb825c9be",
+    "url": "/static/media/logo.2b2eca7f.png"
+  }
+]);
